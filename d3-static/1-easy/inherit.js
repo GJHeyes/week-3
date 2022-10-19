d2c4e140cbf98abb524603442c06dadd781b7445
GJHeyes <gregory-heyes@hotmail.com>
2022-10-20 00:27:59 +0100

    saving my local changes

--- d3-static/1-easy/inherit.js
+++ d3-static/1-easy/inherit.js
@@ -3,8 +3,14 @@ You will have to start with static.js as you will need the Character class.
 
 Define new class called Mage
 */
-const {Character} = require('./static')
+const { Character } = require("./static");
 
+class Mage extends Character {
+  constructor(name) {
+    super(name);
+    this.mana = 100;
+    this.hp = 80;
+  }
+}
 
-
-module.exports = {Mage}
+module.exports = { Mage };
--- d3-static/1-easy/static.js
+++ d3-static/1-easy/static.js
@@ -1,17 +1,16 @@
 class Character {
-    constructor (name) {
-        this.name = name
-        this.xp = 1
-        this.hp = 100
-        this.mana = 50
-        Character.totalCharacters++
-    }
+  static totalCharacters = 0;
 
-    static totalCharacters = 0
+  constructor(name) {
+    this.hp = 100;
+    this.name = name;
+    this.xp = 1;
+    Character.totalCharacters++;
+  }
+
+  get totalCharacters() {
+    return Character.totalCharacters;
+  }
 }
 
-
-
-
-
-module.exports = {Character}
+module.exports = { Character };
